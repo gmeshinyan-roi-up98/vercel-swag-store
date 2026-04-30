7aba2a4f41c319e7dc4c525174c83d71d2e56877
gmeshinyan-roi-up98 <gmeshinyan@roi-up.es>
2026-04-30 18:20:17 +0400

    Add CartQuantityStepper component with styles and constants

---
+++ src/components/atoms/CartQuantityStepper/cartQuantityStepper.const.ts
@@ -0,0 +1,4 @@
+export const CART_QUANTITY_STEPPER_CONSTANTS = {
+  DECREASE_QUANTITY_LABEL: "Decrease quantity",
+  INCREASE_QUANTITY_LABEL: "Increase quantity",
+} as const;
---
+++ src/components/atoms/CartQuantityStepper/index.ts
@@ -0,0 +1,4 @@
+export type { TCartQuantityStepperProps } from "./types";
+export { CartQuantityStepper } from "./CartQuantityStepper";
+export { CART_QUANTITY_STEPPER_CONSTANTS } from "./cartQuantityStepper.const";
+export { classes as cartQuantityStepperClasses } from "./cartQuantityStepperStyles";
