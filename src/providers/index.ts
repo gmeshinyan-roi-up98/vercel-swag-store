739f7e36044bfe308455cc9e1561247aa55fac15
gmeshinyan-roi-up98 <gmeshinyan@roi-up.es>
2026-04-30 18:50:17 +0400

    Add CartHydrators module with components for cart, stock, and notice hydration, including type definitions for props

---
+++ src/providers/index.ts
@@ -0,0 +1,7 @@
+export { CartHydrator, NoticeHydrator, StockHydrator } from "./CartHydrators";
+export { CartHydratorServer } from "./CartServer";
+export type {
+  TCartHydratorProps,
+  TNoticeHydratorProps,
+  TStockHydratorProps,
+} from "./types";
