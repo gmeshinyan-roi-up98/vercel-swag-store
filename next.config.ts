deff79e3b8ee4e21d66232868bbfc96b26428d80
gmeshinyan-roi-up98 <gmeshinyan@roi-up.es>
2026-04-30 18:17:33 +0400

    Add image remote patterns to Next.js config, update package.json dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   cacheComponents: true,
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i8qy5y6gxkdgdcv9.public.blob.vercel-storage.com",
+      },
+    ],
+  },
   typedRoutes: true,
 };
 
