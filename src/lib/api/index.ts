a47e51b623d96a3068f7f03f1f660a3f1d3d0452
gmeshinyan-roi-up98 <gmeshinyan@roi-up.es>
2026-04-30 18:45:20 +0400

    Add index file to export API modules for cart, stock, store, client, products, promotions, and categories

---
+++ src/lib/api/index.ts
@@ -0,0 +1,7 @@
+export * from "./cart";
+export * from "./stock";
+export * from "./store";
+export * from "./client";
+export * from "./products";
+export * from "./promotions";
+export * from "./categories";
